ef861124f6cf4c0255ea26b495297ce10980bd02
mohit-coer <mohit.coer97@gmail.com>
2021-06-04 19:52:12 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import StyledComponent from "./components/StyledComponent/StyledComponent";
+import Navbar from "./containers/Navbar/NavbarComponent"
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <StyledComponent/>
+    </>
+  );
+}
+
+export default App;
